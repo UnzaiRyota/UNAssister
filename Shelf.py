2176ee5a9bc7638eab38d540391e27d707cf2bb6
UnzaiRyota <satellitear@gmail.com>
2016-10-18 10:26:21 +0900

    Add files via upload

---
+++ Shelf.py
@@ -0,0 +1,19 @@
+if __name__ == "__main__":
+    sys.path.append("UNAssister")
+
+    import UNAssister.Setting
+    import UNAssister.CSS
+    import UNAssister.UNAlpha
+    
+    from UNAssister.Setting import*
+    from UNAssister.CSS import *
+    from UNAssister.UNAlpha import *
+        
+
+reload(UNAssister.Setting)
+reload(UNAssister.CSS)
+reload(UNAssister.UNAlpha)
+
+UnAlpha()
+
+
